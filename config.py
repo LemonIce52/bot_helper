098763c18f705ac97b1c9ba29c932a7c7c8bc82c
LemonIce52 <128214825+LemonIce52@users.noreply.github.com>
2024-08-07 22:39:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import re
 
-TOKEN = '6887191350:AAE5bRQoDsqaqWDkjKk6vL4v0DLQ35lvw6g'
-KEY_CIPHER = b'6266e90bd2a4e6ea1d30c5a7e8c6c18b'
+TOKEN = '<your token>'
+KEY_CIPHER = b'<your encryption key>'
 COUNT_OWNER = 1
 CALLBACK_PATTERN_LEFT = re.compile(r'<--:(.*)$')
 CALLBACK_PATTERN_RIGHT = re.compile(r'-->:(.*)$')
